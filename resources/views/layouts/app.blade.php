1578d4c6753ea8a038c36c016cfb1ea19aae1f82
GroverFer <sistemas.grover@gmail.com>
2023-07-24 16:54:30 -0400

    Login and Logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,10 +16,24 @@
             <h1 class="text-3xl font-black">
                 DevStagram
             </h1>
-            <nav class="flex gap-2 items-center">
-                <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
-                <a class="font-bold uppercase text-gray-600 text-sm" href="/crear-cuenta">Crear Cuenta</a>
-            </nav>
+            @auth
+                <nav class="flex gap-2 items-center">
+                    <a class="font-bold text-gray-600 text-sm" href="#">
+                        Hola: <span class="font-normal"> {{ auth()->user()->username }} </span>
+                    </a>
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button type="submit" class="font-bold uppercase text-gray-600 text-sm ">Cerrar Sesión
+                        </button>
+                    </form>
+                </nav>
+            @endauth
+            @guest
+                <nav class="flex gap-2 items-center">
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('login') }}">Login </a>
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('register') }}">Crear Cuenta </a>
+                </nav>
+            @endguest
         </div>
     </header>
 
@@ -30,8 +44,8 @@
         @yield('contenido')
     </main>
 
-    <footer class="text-center p-5 text-gray-500 font-bold uppercase">
-        DevStagram - Todos los derechos reservados {{now()->year}}
+    <footer class=" mt-10 text-center p-5 text-gray-500 font-bold uppercase">
+        DevStagram - Todos los derechos reservados {{ now()->year }}
     </footer>
 
 </body>
